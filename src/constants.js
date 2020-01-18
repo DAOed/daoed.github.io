c620f7cdba3b2b00f84aa6a1df1220e7c2ff37d1
dzivenu <makafuigdzivenu@gmail.com>
2020-01-18 15:06:08 +0000

    fix missing export

--- src/constants.js
+++ src/constants.js
@@ -61,5 +61,5 @@ const avatarSize = "52px";
 const defaultAvatarColors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"];
 
 export {
-  baseBlockstackApi, algorithmWeigth, socialWeigth, constantWeigths, historySize, daoedPrefixer, avatarSize
+  baseBlockstackApi, algorithmWeigth, socialWeigth, constantWeigths, historySize, daoedPrefixer, avatarSize, defaultAvatarColors
 };
